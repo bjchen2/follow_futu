29c9336b55d4389ba69fe40c9e9cbb4e63dadffa
chenxiang <xdu_cx@qq.com>
2025-11-07 20:48:56 +0800

    @update

--- init_param.py
+++ init_param.py
@@ -1,6 +1,8 @@
 us_test_accid = '测试accid'
 us_prod_accid = '生产accid'
 pwd_unlock='你的交易密码'
+hk_test_accid = ''
+hk_prod_accid = ''
 
 #洛阳铲：https://portfolio.futunn.com/portfolio-api/get-portfolio-position?portfolio_id=108131
 #跟踪组合，多空组合
