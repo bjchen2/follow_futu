d625e22d6ac3843e86ebfb3de2049fff57574729
chenxiang <xdu_cx@qq.com>
2025-11-07 20:26:10 +0800

    @update

---
+++ init_param.py
@@ -0,0 +1,7 @@
+us_test_accid = '测试accid'
+us_prod_accid = '生产accid'
+
+#洛阳铲：https://portfolio.futunn.com/portfolio-api/get-portfolio-position?portfolio_id=108131
+#跟踪组合，多空组合
+us_monitor_url = 'https://portfolio.futunn.com/portfolio-api/get-portfolio-position?portfolio_id=154216&language=0'
+hk_monitor_url = ''
